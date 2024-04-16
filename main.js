e44a0ea14b177b86bf5299bf7d0ea654a724272f
Vladislav Pavlov <vldslw@gmail.com>
2024-04-16 13:43:49 -0300

    add reviews section

--- main.js
+++ main.js
@@ -10,4 +10,22 @@ var gallerySlider = new Splide("#gallerySlider", {
     speed: 0.3,
   },
 });
+
+var reviewsSlider = new Splide("#reviewsSlider", {
+  perPage: 3,
+  gap: "32px",
+  pagination: false,
+  // type: "loop",
+  // drag: "free",
+  // focus: "center",
+  // gap: "32px",
+  // arrows: false,
+
+  // perPage: 3,
+  // autoScroll: {
+  //   speed: 0.3,
+  // },
+});
+
 gallerySlider.mount(window.splide.Extensions);
+reviewsSlider.mount();
