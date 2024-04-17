ea907f6c1e7861192e02d8f398a332a04f42bbc9
Vladislav Pavlov <vldslw@gmail.com>
2024-04-17 13:34:07 -0300

    fix css on mobile screen

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 var gallerySlider = new Splide("#gallerySlider", {
   height: "240px",
+  breakpoints: {
+    768: {
+      height: "160px",
+      perPage: 2,
+    },
+    420: {
+      perPage: 1,
+    },
+  },
+  cover: true,
   type: "loop",
   drag: "free",
   focus: "center",
@@ -14,18 +24,13 @@ var gallerySlider = new Splide("#gallerySlider", {
 
 var reviewsSlider = new Splide("#reviewsSlider", {
   perPage: 3,
+  breakpoints: {
+    768: {
+      perPage: 1,
+    },
+  },
   gap: "32px",
   pagination: false,
-  // type: "loop",
-  // drag: "free",
-  // focus: "center",
-  // gap: "32px",
-  // arrows: false,
-
-  // perPage: 3,
-  // autoScroll: {
-  //   speed: 0.3,
-  // },
 });
 
 gallerySlider.mount(window.splide.Extensions);
