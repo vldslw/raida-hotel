449f1adbbfe20a29b9e6411561fe9a4257e623a9
Vladislav Pavlov <vldslw@gmail.com>
2024-04-20 17:23:33 -0300

    add room details section

--- scripts/main.js
+++ scripts/main.js
@@ -35,5 +35,40 @@ var reviewsSlider = new Splide("#reviewsSlider", {
   pagination: false,
 });
 
+var roomSliderMain = new Splide("#roomSliderMain", {
+  type: "fade",
+  fixedHeight: 440,
+  // heightRatio: 0.5,
+  pagination: false,
+  arrows: false,
+  cover: true,
+});
+
+var roomSliderThumbnails = new Splide("#roomSliderThumbnails", {
+  rewind: true,
+  perPage: 3,
+  heightRatio: 0.22,
+  isNavigation: true,
+  gap: 16,
+  focus: "center",
+  pagination: false,
+  cover: true,
+  dragMinThreshold: {
+    mouse: 4,
+    touch: 10,
+  },
+  breakpoints: {
+    768: {
+      perPage: 1,
+      type: "loop",
+      heightRatio: 0.67,
+      padding: { left: "0", right: "80px" },
+    },
+  },
+});
+
 gallerySlider.mount(window.splide.Extensions);
 reviewsSlider.mount();
+roomSliderMain.sync(roomSliderThumbnails);
+roomSliderMain.mount();
+roomSliderThumbnails.mount();
