b413868c909d41dab807e6282e70187cfb86bfaf
Vladislav Pavlov <vldslw@gmail.com>
2024-04-21 00:39:02 -0300

    add js to open and close room details cards

--- scripts/main.js
+++ scripts/main.js
@@ -35,40 +35,65 @@ var reviewsSlider = new Splide("#reviewsSlider", {
   pagination: false,
 });
 
-var roomSliderMain = new Splide("#roomSliderMain", {
-  type: "fade",
-  fixedHeight: 440,
-  // heightRatio: 0.5,
-  pagination: false,
-  arrows: false,
-  cover: true,
-});
-
-var roomSliderThumbnails = new Splide("#roomSliderThumbnails", {
-  rewind: true,
-  perPage: 3,
-  heightRatio: 0.22,
-  isNavigation: true,
-  gap: 16,
-  focus: "center",
-  pagination: false,
-  cover: true,
-  dragMinThreshold: {
-    mouse: 4,
-    touch: 10,
-  },
-  breakpoints: {
-    768: {
-      perPage: 1,
-      type: "loop",
-      heightRatio: 0.67,
-      padding: { left: "0", right: "80px" },
-    },
-  },
-});
-
 gallerySlider.mount(window.splide.Extensions);
 reviewsSlider.mount();
-roomSliderMain.sync(roomSliderThumbnails);
-roomSliderMain.mount();
-roomSliderThumbnails.mount();
+
+const cards = document.querySelectorAll(".room-details__card");
+
+cards.forEach((card) => {
+  let roomSliderMain;
+  let roomSliderThumbnails;
+
+  const button = card.querySelector(".room-details__open-button");
+
+  button.addEventListener("click", function () {
+    const lower = card.querySelector(".room-details__lower");
+    lower.classList.toggle("room-details__lower_open");
+
+    if (
+      lower.classList.contains("room-details__lower_open") &&
+      !roomSliderMain &&
+      !roomSliderThumbnails
+    ) {
+      const mainSliderElement = card.querySelector(
+        ".room-details__slider_type_main"
+      );
+      const thumbnailSliderElement = card.querySelector(
+        ".room-details__slider_type_thumbnails"
+      );
+
+      roomSliderMain = new Splide(mainSliderElement, {
+        type: "fade",
+        fixedHeight: 440,
+        pagination: false,
+        arrows: false,
+        cover: true,
+      }).mount();
+
+      roomSliderThumbnails = new Splide(thumbnailSliderElement, {
+        rewind: true,
+        perPage: 3,
+        heightRatio: 0.22,
+        isNavigation: true,
+        gap: 16,
+        focus: "center",
+        pagination: false,
+        cover: true,
+        dragMinThreshold: {
+          mouse: 4,
+          touch: 10,
+        },
+        breakpoints: {
+          768: {
+            perPage: 1,
+            type: "loop",
+            heightRatio: 0.67,
+            padding: { left: "0", right: "80px" },
+          },
+        },
+      }).mount();
+
+      roomSliderMain.sync(roomSliderThumbnails);
+    }
+  });
+});
