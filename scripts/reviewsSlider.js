b83ac68c078a25fc928073d0e19148edb80e6b1a
Vladislav Pavlov <vldslw@gmail.com>
2024-05-06 01:51:51 -0300

    add hover style to review cards

--- scripts/reviewsSlider.js
+++ scripts/reviewsSlider.js
@@ -1,14 +1,15 @@
 var reviewsSlider = new Splide("#reviewsSlider", {
   perPage: 3,
-  height: "420px",
+  padding: {
+    right: "48px",
+  },
   breakpoints: {
     768: {
-      height: "342px",
       perPage: 1,
       padding: { left: "18px", right: "56px" },
     },
   },
-  gap: "32px",
+  gap: "16px",
   pagination: false,
 });
 
