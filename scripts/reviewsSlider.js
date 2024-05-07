543f58df66b09b46324dcbf857752f8a2112eb14
Vladislav Pavlov <vldslw@gmail.com>
2024-05-07 00:21:53 -0300

    add opening review card on the whole screen on mobile

--- scripts/reviewsSlider.js
+++ scripts/reviewsSlider.js
@@ -13,4 +13,49 @@ var reviewsSlider = new Splide("#reviewsSlider", {
   pagination: false,
 });
 
+reviewsSlider.on("mounted", () => {
+  document.querySelectorAll(".reviews__card").forEach((card) => {
+    console.log("added");
+    let parent = card.parentElement;
+    let nextSibling = card.nextSibling;
+
+    let openCardButton = card.querySelector(".reviews__open-card");
+    let closeCardButton = card.querySelector(".reviews__close-card");
+    if (openCardButton) {
+      openCardButton.addEventListener("click", () => {
+        console.log("click");
+        document.body.appendChild(card);
+        card.classList.add("reviews__card_expanded");
+        openCardButton.classList.add("reviews__open-card_hidden");
+        closeCardButton.classList.add("reviews__close-card_active");
+      });
+    } else {
+      console.error("Open card button not found");
+    }
+
+    if (closeCardButton) {
+      closeCardButton.addEventListener("click", closeCard);
+      card.addEventListener("touchmove", (event) => {
+        if (event.touches[0].clientY > window.innerHeight / 2) {
+          closeCard();
+        }
+      });
+    } else {
+      console.error("Close card button not found");
+    }
+
+    function closeCard() {
+      console.log("close click");
+      card.classList.remove("reviews__card_expanded");
+      openCardButton.classList.remove("reviews__open-card_hidden");
+      closeCardButton.classList.remove("reviews__close-card_active");
+      if (nextSibling) {
+        parent.insertBefore(card, nextSibling);
+      } else {
+        parent.appendChild(card);
+      }
+    }
+  });
+});
+
 reviewsSlider.mount();
