45b94d79eab1feb6bbee90c0bbb37382f38b1d72
Vladislav Pavlov <vladislawpavlov@gmail.com>
2024-05-26 15:28:09 -0300

    remake burger menu button functionelity

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,14 @@
-const openMenuButtons = document.querySelectorAll(".header__menu-button");
-const closeMenuButtons = document.querySelectorAll(".menu__close-button");
+const openMenuButtons = document.querySelectorAll(".header__ham-menu");
 
 openMenuButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    button.classList.toggle("header__ham-menu_active");
     const menu = document.querySelector(button.dataset.menuTarget);
     openMenu(menu);
   });
 });
 
-closeMenuButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const menu = button.closest(".menu");
-    closeMenu(menu);
-  });
-});
-
 function openMenu(menu) {
   if (menu == null) return;
-  menu.classList.add("menu_active");
-}
-
-function closeMenu(menu) {
-  if (menu == null) return;
-  menu.classList.remove("menu_active");
+  menu.classList.toggle("menu_active");
 }
